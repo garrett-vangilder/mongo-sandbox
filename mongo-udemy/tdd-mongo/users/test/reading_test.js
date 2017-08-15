80c7bacc906fa94f4edbce1cf0170cf5a4be870d
Garrett Vangilder <garrett.vangilder@gmail.com>
2017-08-15 09:33:40 -0400

    two tests added

--- mongo-udemy/tdd-mongo/users/test/reading_test.js
+++ mongo-udemy/tdd-mongo/users/test/reading_test.js
@@ -13,7 +13,15 @@ describe('Reading users out of the database', () => {
   it('finds all users with the name of joe', (done) => {
     User.find({ name: 'Joe' })
       .then((users) => {
-        console.log(users);
+        assert(users[0]._id.toString() === joe._id.toString());
+        done();
+      });
+  });
+  
+  it('finds a user with a particular id', (done) => {
+    User.findOne({ _id: joe._id })
+      .then((user) => {
+        assert(user.name === "Joe");
         done();
       });
   });
