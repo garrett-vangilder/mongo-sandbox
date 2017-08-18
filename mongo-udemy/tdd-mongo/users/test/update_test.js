5fa42fabf4399c0fc2081e46288b514f6c021259
Garrett Vangilder <garrett.vangilder@gmail.com>
2017-08-18 12:32:23 -0400

    adds virtual fields

--- mongo-udemy/tdd-mongo/users/test/update_test.js
+++ mongo-udemy/tdd-mongo/users/test/update_test.js
@@ -5,7 +5,7 @@ describe('Updates a user', () => {
   let joe;
 
   beforeEach((done) => {
-    joe = new User({ name: 'Joe', postCount: 0 });
+    joe = new User({ name: 'Joe', likes: 0 });
     joe.save()
       .then(() => done());
   });
@@ -34,11 +34,11 @@ describe('Updates a user', () => {
     assertName(User.findByIdAndUpdate({ _id: joe._id }, { name: 'Mike' }), done);
   });
 
-  it('A user can have their post count incremented by 1', (done) => {
-    User.update({ name: 'Joe' }, { $inc: { postCount: 1 } })
+  it('A user can have their likes incremented by 1', (done) => {
+    User.update({ name: 'Joe' }, { $inc: { likes: 1 } })
       .then(() => User.findOne({ name: 'Joe' }))
       .then((user) => {
-        assert(user.postCount === 1);
+        assert(user.likes === 1);
         done()
       });
     
